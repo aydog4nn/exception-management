2f463b306013424565d7b5e4fad942fa5a9d7b54
aydog4nn <zephyrflaner@gmail.com>
2026-03-04 09:41:12 +0300

    added-schedule-example

---
+++ src/main/java/com/ahmetaydo/scheduled/ScheduleExample.java
@@ -0,0 +1,18 @@
+package com.ahmetaydo.scheduled;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleExample {
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void write1To10(){
+        for (int i = 1;i<=10;i++){
+            System.out.print(i + " ");
+        }
+    }
+
+
+}
--- src/main/java/com/ahmetaydo/starter/ExceptionManagmentStarter.java
+++ src/main/java/com/ahmetaydo/starter/ExceptionManagmentStarter.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackages = {"com.ahmetaydo"})
 @EnableJpaRepositories(basePackages = {"com.ahmetaydo"})
 @ComponentScan(basePackages = {"com.ahmetaydo"})
 @SpringBootApplication
+@EnableScheduling
 public class ExceptionManagmentStarter {
 
 	public static void main(String[] args) {
